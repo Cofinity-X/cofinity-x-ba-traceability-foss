e7155e63bec28f438208173584055d44436adc22
Ram Narayan Mani <ram-narayan.mani@cofinity-x.com>
2023-10-11 13:04:03 +0200

    feature:DO-1264:added miw helm chart

--- charts/traceability-foss/forwardingPorts.sh
+++ charts/traceability-foss/forwardingPorts.sh
@@ -4,7 +4,8 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Port forwarding${NC}"
-kubectl port-forward svc/edc-vault 8200:8200 -n irs &
+kubectl port-forward svc/tracex-app-postgresql 5432:5432 -n tracex &
+: 'kubectl port-forward svc/edc-vault 8200:8200 -n irs &
 kubectl port-forward svc/edc-daps 4567:4567 -n irs &
 kubectl port-forward svc/edc-provider-control-plane 6080:8080 6181:8181 -n irs &
 kubectl port-forward svc/edc-consumer-control-plane 7080:8080 7181:8181 -n irs &
@@ -16,3 +17,4 @@ kubectl port-forward svc/irs-provider-backend 10199:8080 -n irs &
 kubectl port-forward svc/irs-minio-console 9001:9001 -n irs &
 kubectl port-forward svc/irs 8080:8080 -n irs &
 kubectl port-forward svc/irs-frontend 3000:8080 -n irs
+'
