f044f530da76f46dc5fbef46ab871f6a3591d9cc
Heinrich Götzger EXT <Heinrich.Goetzger@partner.doubleslash.de>
2023-11-02 11:29:32 +0100

    feature:DO-4024:Finetuning of testdata

--- tx-backend/src/main/resources/db/testdata/afterMigrate__044_assets_as_planned_investigations.sql
+++ tx-backend/src/main/resources/db/testdata/afterMigrate__044_assets_as_planned_investigations.sql
@@ -3,9 +3,11 @@
 -- it is only intended for usage in local or test environments
 
 insert into assets_as_planned_investigations
-    (investigation_id, asset_id)
+    (investigation_id   , asset_id)
 values
     (${investigationId2}, ${assetAsPlannedId1}),
     (${investigationId4}, ${assetAsPlannedId3});
 
-
+update assets_as_built
+    set in_investigation = true
+    where id in (${assetAsPlannedId1}, ${assetAsPlannedId3}); -- incoming and outgoing
