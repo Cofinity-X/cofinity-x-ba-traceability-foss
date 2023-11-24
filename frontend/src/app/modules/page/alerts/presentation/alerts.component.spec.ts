5c10a65fb98a3df66b145f45262e7e3c4d926f78
Joel Dietz <145654504+JoelDietz@users.noreply.github.com>
2023-11-24 11:55:52 +0100

    [DO- 4505][patch] sorting tests relocation (#61)
    * fix: changed the testing to only test what is in the component since the rendering could take some time in the tests.
    
    * chore: clean up of the old test that were commented out so that in case of errors the old test could be checked

--- frontend/src/app/modules/page/alerts/presentation/alerts.component.spec.ts
+++ frontend/src/app/modules/page/alerts/presentation/alerts.component.spec.ts
@@ -27,16 +27,6 @@ import { AlertsComponent } from './alerts.component';
 import { TableEventConfig } from '@shared/components/table/table.model';
 
 describe('AlertsComponent', () => {
-  var originalTimeout: number;
-  beforeEach(function () {
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  });
-
-  afterEach(function () {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-  });
-
   const renderAlerts = async () => {
     return await renderComponent(AlertsComponent, {
       imports: [AlertsModule],
