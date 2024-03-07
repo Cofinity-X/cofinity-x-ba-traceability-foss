aa9259f74bf71621f48e629250dc434fb87cd56e
Viann Bresler <viann.bresler@accso.de>
2024-03-07 13:30:56 +0100

    chore:[BA-22] Clean up

--- cfx-frontend/src/app/modules/shared/components/table-settings/table-settings.component.spec.ts
+++ cfx-frontend/src/app/modules/shared/components/table-settings/table-settings.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TableSettingsComponent } from './table-settings.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { SharedModule } from '@shared/shared.module';
+import { of } from 'rxjs';
+import { I18NextModule } from 'angular-i18next';
 
+class MatDialogMock {
+  open() {
+    return {
+      afterClosed: () => of(true),
+    };
+  }
+
+  closeAll() {
+    // Mock implementation
+  }
+}
 describe('TableSettingsComponent', () => {
   let component: TableSettingsComponent;
   let fixture: ComponentFixture<TableSettingsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TableSettingsComponent]
+      declarations: [TableSettingsComponent],
+      imports: [
+        SharedModule,
+        I18NextModule.forRoot(),
+      ],
+      providers: [
+        { provide: MatDialog, useClass: MatDialogMock },
+        {
+          provide: MatDialogRef,
+          useValue: {
+            close: () => { },
+          },
+        },
+      ]
     })
       .compileComponents();
 
