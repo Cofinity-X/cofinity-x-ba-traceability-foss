7969958343c519331aae83249e37a9f0e7b7e5d2
Pooja Patel <poojapatelce@gmail.com>
2024-03-14 18:13:12 +0100

    [BA-790][minor] Reactivate correct sorting of status (alerts/investigations)
    - Modified sorting functionality
    - Modified testcase result according sorting functionality

--- tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/qualitynotification/alert/ReadCreatedAlertsInSortedOrderControllerIT.java
+++ tx-backend/src/test/java/org/eclipse/tractusx/traceability/integration/qualitynotification/alert/ReadCreatedAlertsInSortedOrderControllerIT.java
@@ -128,7 +128,7 @@ class ReadCreatedAlertsInSortedOrderControllerIT extends IntegrationTestSpecific
                 .body("pageSize", Matchers.is(10))
                 .body("content", Matchers.hasSize(4))
                 .body("totalItems", Matchers.is(4))
-                .body("content.status", Matchers.containsInRelativeOrder("ACCEPTED", "ACCEPTED", "CREATED", "SENT"));
+                .body("content.status", Matchers.containsInRelativeOrder("CREATED", "SENT", "ACCEPTED", "ACCEPTED"));
     }
 
     @Test
