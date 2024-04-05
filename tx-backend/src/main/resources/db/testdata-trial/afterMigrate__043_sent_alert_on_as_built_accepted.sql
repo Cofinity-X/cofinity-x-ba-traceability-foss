b4962103c07d88bb6b3b5053258b5b5a825daa21
Heinrich Götzger <145972012+hgo-ds@users.noreply.github.com>
2024-04-05 16:03:11 +0200

    Chore/ba 792 trx test data for trial version (#206)
    * BA-792 TRX_Test Data for Trial Version
    
    initial introduction of administrative- and configuration files
    initial introduction of truncate and asBuilt assets
    fix outdated truncate for all test setups (caused by migration from upstream)
    refactor ownBpn configuration to get single source
    initial introduction of parent/child relations
    adjust short-id and naming
    fixed typo
    add notifications
    ---------
    
    Co-authored-by: Pooja Patel <poojapatelce@gmail.com>

---
+++ tx-backend/src/main/resources/db/testdata-trial/afterMigrate__043_sent_alert_on_as_built_accepted.sql
@@ -0,0 +1,50 @@
+-- this script is executed by flyway as part of a lifecycle hook after all migrations have been applied
+-- (see https://documentation.red-gate.com/fd/callback-concept-184127466.html).
+-- it is only intended for usage in local or test environments
+
+-- This creates an alert in state ACCEPTED in Severity Minor for asBuilt asset Control Unit big which is sent from BPNL000000000001 to BPNL000CUSTOMER2
+
+---
+insert into alert
+    (id             , bpn      , close_reason, created                              , description                   , status    , side    , accept_reason                             , decline_reason, updated, error_message)
+values
+    (${alertSentId4}, ${bpnOwn}, null        , current_timestamp - interval '5 days', 'Alert about Control Unit big', 'ACCEPTED', 'SENDER', 'Thanks for letting us know. We''ll check', null          , null   , null);
+
+---
+-- reset sequence to highest next-val
+select setval('alert_id_seq1', (select max(a.id) from alert a), true);
+
+---
+-- initial message
+insert into alert_notification
+    (id                          , alert_id       , contract_agreement_id, edc_url, notification_reference_id             , created_by, send_to        , target_date                           , severity, created_by_name, send_to_name       , edc_notification_id                   , status   , created                              , updated          , message_id, is_initial)
+values
+    (${alertNotificationSentId4a}, ${alertSentId4}, 'contractAgreementId', null   , '8e777e64-f7f3-44e2-b78e-75da964f1531', ${bpnOwn} , ${bpnCustomer2}, current_timestamp + interval '1 month', 'MINOR' , ${bpnOwnName}  , ${bpnCustomer2Name}, '8e777e64-f7f3-44e2-b78e-75da964f1531', 'CREATED', current_timestamp - interval '5 days', current_timestamp, null      , true);
+
+---
+-- join initial notification to asset
+insert into asset_as_built_alert_notifications
+    (alert_notification_id       , asset_id)
+values
+    (${alertNotificationSentId4a}, ${assetAsBuiltId16});
+
+---
+-- join alert to asset
+insert into assets_as_built_alerts
+    (alert_id       , asset_id)
+values
+    (${alertSentId4}, ${assetAsBuiltId16});
+
+---
+-- ACCEPTED by receiver notification message
+insert into alert_notification
+    (id                          , alert_id       , contract_agreement_id, edc_url, notification_reference_id             , created_by     , send_to  , target_date                           , severity, created_by_name    , send_to_name , edc_notification_id                   , status    , created                              , updated          , message_id, is_initial)
+values
+    (${alertNotificationSentId4b}, ${alertSentId4}, 'contractAgreementId', null   , '8e777e64-f7f3-44e2-b78e-75da964f1531', ${bpnCustomer2}, ${bpnOwn}, current_timestamp + interval '1 month', 'MINOR' , ${bpnCustomer2Name}, ${bpnOwnName}, '8e777e64-f7f3-44e2-b78e-75da964f1531', 'ACCEPTED', current_timestamp - interval '3 days', current_timestamp, null      , false);
+
+---
+-- join ACCEPTED notification to asset
+insert into asset_as_built_alert_notifications
+    (alert_notification_id       , asset_id)
+values
+    (${alertNotificationSentId4b}, ${assetAsBuiltId16});
---
+++ tx-backend/src/main/resources/db/testdata-trial/afterMigrate__064_sent_investigation_on_as_built_declined.sql
@@ -0,0 +1,49 @@
+-- this script is executed by flyway as part of a lifecycle hook after all migrations have been applied
+-- (see https://documentation.red-gate.com/fd/callback-concept-184127466.html).
+-- it is only intended for usage in local or test environments
+
+-- This creates an investigation in state DECLINED in Severity Major for asBuilt asset CableCraft Electric Fuse big which is sent from BPNL000000000001 to BPNL000SUPPLIER3
+
+---
+insert into investigation
+    (id                     , bpn      , close_reason, created                              , description                                             , status    , side    , accept_reason, decline_reason    , updated                              , error_message)
+values
+    (${investigationSentId5}, ${bpnOwn}, null        , current_timestamp - interval '5 days', 'Investigation on Electric Fuse big due to broken wire.', 'DECLINED', 'SENDER', null         , 'It''s not broken', current_timestamp - interval '1 hour', null);
+
+---
+-- reset sequence to highest next-val
+select setval('investigation_id_seq1', (select max(i.id) from investigation i), true);
+
+---
+insert into investigation_notification
+    (id                                  , contract_agreement_id, edc_url                    , notification_reference_id, created_by, send_to        , investigation_id       , target_date                           , severity, created_by_name, send_to_name       , edc_notification_id                   , status    , created                              , updated                              , message_id                            , is_initial)
+values
+    (${investigationNotificationSentId5a}, 'contractAgreementId', 'http://localhost:5001/edc', null                     , ${bpnOwn} , ${bpnSupplier3}, ${investigationSentId5}, current_timestamp + interval '2 weeks', 'MAJOR' , ${bpnOwnName}  , ${bpnSupplier3Name}, '3ac2239a-e63f-4c19-b3b3-e6a2e5a240da', 'DECLINED', current_timestamp - interval '5 days', current_timestamp - interval '1 hour', '749b31e9-9e73-4699-9470-dbee67ebc7a7', true);
+
+---
+-- join investigation to asset
+insert into assets_as_built_notifications
+    (notification_id                     , asset_id)
+values
+    (${investigationNotificationSentId5a}, ${assetAsBuiltId08});
+
+---
+-- join investigation to asset
+insert into assets_as_built_investigations
+    (investigation_id       , asset_id)
+values
+    (${investigationSentId5}, ${assetAsBuiltId08});
+
+---
+-- DECLINED by receiver notification message
+insert into investigation_notification
+    (id                                  , contract_agreement_id, edc_url                    , notification_reference_id, created_by     , send_to  , investigation_id       , target_date                           , severity, created_by_name    , send_to_name , edc_notification_id                   , status    , created                              , updated                              , message_id                            , is_initial)
+values
+    (${investigationNotificationSentId5b}, 'contractAgreementId', 'http://localhost:5001/edc', null                     , ${bpnSupplier3}, ${bpnOwn}, ${investigationSentId5}, current_timestamp + interval '2 weeks', 'MAJOR' , ${bpnSupplier3Name}, ${bpnOwnName}, '8925f21f-09eb-4789-81fb-ec221e9e1561', 'DECLINED', current_timestamp - interval '5 days', current_timestamp - interval '1 hour', '207ba6cf-217b-401d-a5da-69cac8b154a5', false);
+
+---
+-- join DECLINED notification to asset
+insert into assets_as_built_notifications
+    (notification_id                     , asset_id)
+values
+    (${investigationNotificationSentId5b}, ${assetAsBuiltId08});
