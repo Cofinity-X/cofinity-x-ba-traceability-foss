a93585327135cd420afa9e7fb96099b6bf3870d2
Pooja Patel <poojapatelce@gmail.com>
2024-05-10 16:52:21 +0200

    [chore] BA-986 Create data migration script
     - Updated data migration script

--- tx-backend/src/main/resources/db/migration/V22__merge_alert_and_notification_tables.sql
+++ tx-backend/src/main/resources/db/migration/V22__merge_alert_and_notification_tables.sql
@@ -84,20 +84,56 @@ CREATE TABLE public.assets_as_built_notification_messages
 
 -- transfer data from old table to new tables
 -- Insert data from alert to notification table
-INSERT INTO notification (bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, "type")
-SELECT bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, 'ALERT' AS "type"
+INSERT INTO notification (id,bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, "type")
+SELECT id,bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, 'ALERT' AS "type"
 FROM alert;
 
--- Insert data from temp_investigation to notification table
-INSERT INTO notification(bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, "type")
-SELECT bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, 'INVESTIGATION' AS "type"
-FROM investigation;
 
 -- Insert data from alert_notification to notification_message table
 INSERT INTO notification_message
 (id       , contract_agreement_id, notification_reference_id, send_to, created_by, notification_id            , target_date, created_by_name, send_to_name, edc_notification_id, "status", created, updated, error_message, message_id, "severity")
 select  id, contract_agreement_id, notification_reference_id, send_to, created_by, alert_id as notification_id, target_date, created_by_name, send_to_name, edc_notification_id, "status", created, updated, error_message, message_id, "severity"
 from alert_notification;
+
+
+-- Create a sequence starting from 1
+CREATE SEQUENCE investigation_uid_seq_temp;
+
+-- Set the starting value of the sequence to be the next value after the maximum UID found in the 'notification' table
+SELECT setval('investigation_uid_seq_temp', COALESCE((SELECT MAX(id) FROM notification), 0) + 1);
+
+CREATE TABLE investigation_id_history (
+                                          old_investigation_id INT NOT NULL,
+                                          new_investigation_id INT NOT NULL
+);
+
+INSERT INTO investigation_id_history (old_investigation_id,new_investigation_id)
+SELECT id,nextval('investigation_uid_seq_temp')
+FROM investigation;
+
+-- Drop the foreign key constraint
+ALTER TABLE assets_as_built_investigations DROP CONSTRAINT fk_investigation;
+ALTER TABLE investigation_notification DROP CONSTRAINT fk_investigation;
+
+UPDATE investigation
+SET id = investigation_id_history.new_investigation_id
+    FROM investigation_id_history
+WHERE investigation.id = investigation_id_history.old_investigation_id;
+
+UPDATE assets_as_built_investigations
+SET investigation_id = investigation_id_history.new_investigation_id
+    FROM investigation_id_history
+WHERE assets_as_built_investigations.investigation_id = investigation_id_history.old_investigation_id;
+
+UPDATE investigation_notification
+SET investigation_id = investigation_id_history.new_investigation_id
+    FROM investigation_id_history
+WHERE investigation_notification.investigation_id = investigation_id_history.old_investigation_id;
+
+-- Insert data from investigation to notification table
+INSERT INTO notification(id,bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, "type")
+SELECT id,bpn, close_reason, created, description, "status", side, accept_reason, decline_reason, updated, 'INVESTIGATION' AS "type"
+FROM investigation;
 
 -- Insert data from investigation_notification to notification_message table
 INSERT INTO notification_message
@@ -135,5 +171,6 @@ DROP TABLE public.alert_notification;
 DROP TABLE public.assets_as_built_alerts;
 DROP TABLE public.alert;
 DROP TABLE public.investigation;
+DROP TABLE public.investigation_id_history;
 DROP SEQUENCE public.alert_id_seq;
 DROP SEQUENCE public.investigation_id_seq;
